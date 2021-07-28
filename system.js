96ef77a95c8dd00e2145876786dbd0f5a589be48
WolfGangS <WolfGangS@users.noreply.github.com>
2021-07-28 04:24:11 +0000

    deploy: b28927301a1d79654d0e3e54d3e044f1c71122d3

--- system.js
+++ system.js
@@ -468,10 +468,12 @@ const queueWrite = () => {
 };
 const _getItem = Storage.prototype.getItem;
 const getItem = (key) => {
-    if (_cache.hasOwnProperty(key)) {
-        return _cache[key];
+    if (!_cache.hasOwnProperty(key)) {
+        const get = _getItem.apply(window.localStorage, [key]);
+        if (get != null)
+            _cache[key] = get;
     }
-    return _getItem.apply(window.localStorage, [key]);
+    return _cache[key] || null;
 };
 Storage.prototype.setItem = (_key, _value) => {
     throw "Blocked";
